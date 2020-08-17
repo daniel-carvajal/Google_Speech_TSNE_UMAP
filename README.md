51a32cfee2ecad98b78a0c06bacea9574274e830
dandotwave <danielcarvajal1295@gmail.com>
2020-08-17 00:16:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Google_Speech_TSNE_UMAP [React Build]
+
+Dimensionality reduction (`T-SNE` and `UMAP`) applied to selected samples from [Google's Speech Commands Dataset](https://ai.googleblog.com/2017/08/launching-speech-commands-dataset.html)
+
+
+## About
+
+- Original inspiration and method for audio processing [here](http://doc.gold.ac.uk/~lfedd001/three/demo.html).
+- [Code](https://corticoai.github.io/3d-react-demo/) modified to render audio data with React and react-three-fiber.
+- This experiment uses the first `90 audio samples` of each word from the dataset.
+
+
+
+
---
+++ precache-manifest.4aa711f5b42c9dbcd0683537bd606edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b8288f693ebb979a5487d356b52783",
+    "url": "/Google_Speech_TSNE_UMAP/index.html"
+  },
+  {
+    "revision": "5f6bc3a702b4ae57da3e",
+    "url": "/Google_Speech_TSNE_UMAP/static/css/main.5c2b4f7b.chunk.css"
+  },
+  {
+    "revision": "21286f80d4c38684dc0e",
+    "url": "/Google_Speech_TSNE_UMAP/static/js/2.0bb5eb57.chunk.js"
+  },
+  {
+    "revision": "f90fa31674e65824567a5d04954ec034",
+    "url": "/Google_Speech_TSNE_UMAP/static/js/2.0bb5eb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6bc3a702b4ae57da3e",
+    "url": "/Google_Speech_TSNE_UMAP/static/js/main.b3a8f882.chunk.js"
+  },
+  {
+    "revision": "cfc639dd6053e94b980e",
+    "url": "/Google_Speech_TSNE_UMAP/static/js/runtime-main.e609109d.js"
+  }
+]);
